b73833d8e33d9a7cecb5ba7697a2b52b9c67acfb
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-07-25 22:48:41 -0600

    Update zerofour.py

--- zerofour.py
+++ zerofour.py
@@ -6,24 +6,55 @@ import json
 import re
 import sys
 from rich.console import Console
-from home_nmap import query
-from home_nmap import OutputParser
-from home_nmap import fill_simple_table
+# from home_nmap.home_nmap import query
+# from home_nmap import OutputParser
+# from home_nmap import fill_simple_table
 
-scanner = nmap.PortScanner()
+nm = nmap.PortScanner()
 
-target = "scanme.nmap.org"
+print("<<=============================================>>")
 
-scanner.scan(target)
+ipaddr = input("Please enter the IP address and range to scan for devices on your local network: ")
+print("The IP/range you entered is: ", ipaddr)
 
-for host in scanner.all_hosts():
-    print("Host: ", host)
-    print("State: ", scanner[host].state())
-    for proto in scanner[host].all_protocols():
-        print("Protocol: ", proto)
-        ports = scanner[host][proto].keys()
+options = "-sn"
+
+print("Starting the scan...")
+nm.scan(ipaddr, arguments='-sP' )
+nm.command_line()
+
+print(nm.scaninfo())
+
+for host in nm.all_hosts():
+    if nm[host].state() == 'up':
+        print('Host: %s' % host)
+
+print("\nScan results:")
+for host in nm.all_hosts():
+    print(f"\nHost: {host}")
+    
+    hostnames = nm[host].hostname()
+    print(f"Hostname: {hostnames if hostnames else 'No hostname'}")
+    
+    state = nm[host].state()
+    print(f"State: {state}")
+    
+    if 'addresses' in nm[host] and 'mac' in nm[host]['addresses']:
+        mac_address = nm[host]['addresses']['mac']
+        print(f"MAC Address: {mac_address}")
+    else:
+        print("MAC Address: No MAC address found")
+    
+    if 'hostscript' in nm[host]:
+        for script in nm[host]['hostscript']:
+            print(f"Script: {script['id']}, Output: {script['output']}")
+    
+    for protocol in nm[host].all_protocols():
+        print(f"Protocol: {protocol}")
+        ports = nm[host][protocol].keys()
         for port in ports:
-            print("Port: ", port, "State: ", scanner[host][proto][port]['state'])
+            print(f"Port: {port}\tState: {nm[host][protocol][port]['state']}")
+            
 #########################
 # urlstr = input("Ingresa la url para acceder a tu modem: \n")
 # username = input("Ingresa el nombre de usuario para acceder a tu modem: \n")
